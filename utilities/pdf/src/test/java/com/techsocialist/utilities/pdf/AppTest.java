fce56deefe1b0fabe43fc1795fa3fb16d8c6e8cf
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-06-05 13:16:41 +0530

    add pdf module in utilities

---
+++ utilities/pdf/src/test/java/com/techsocialist/utilities/pdf/AppTest.java
@@ -0,0 +1,20 @@
+package com.techsocialist.utilities.pdf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
