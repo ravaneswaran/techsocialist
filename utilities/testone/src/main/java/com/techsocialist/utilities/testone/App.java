53b8631aa2ed42e9fcdd6f461e4812e0d3295804
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-06-06 17:58:25 +0530

    open braces included

---
+++ utilities/testone/src/main/java/com/techsocialist/utilities/testone/App.java
@@ -0,0 +1,13 @@
+package com.techsocialist.utilities.testone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+
+        System.out.println( "Hello World!" );
+    }
+}
