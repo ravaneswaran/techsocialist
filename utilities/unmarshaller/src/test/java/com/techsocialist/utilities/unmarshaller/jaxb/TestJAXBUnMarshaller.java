4610ac2d04d8c8dcf15e614adbc2b591a887acd1
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-06-06 23:57:36 +0530

    marshaller and unmarshaller modules included

---
+++ utilities/unmarshaller/src/test/java/com/techsocialist/utilities/unmarshaller/jaxb/TestJAXBUnMarshaller.java
@@ -0,0 +1,24 @@
+package com.techsocialist.utilities.unmarshaller.jaxb;
+
+import com.techsocialist.utilities.marshaller.jaxb.JAXBMarshaller;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import javax.xml.bind.JAXBException;
+
+/**
+ * @author Ravaneswaran Chinnasamy
+ *
+ */
+public class TestJAXBUnMarshaller extends TestCase {
+
+	public void testDuisUnMarshaller() throws JAXBException {
+		TestEntity testEntity = new TestEntity();
+		String xmlString = JAXBMarshaller.marshall(testEntity);
+		Object object = JAXBUnMarshaller.unMarshall(TestEntity.class,
+				xmlString);
+		Assert.assertNotNull(object);
+		Assert.assertTrue(object instanceof TestEntity);
+	}
+
+}
