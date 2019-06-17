bfc68e3024d405afd7fe0919f5be9a6a80d0c3b4
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-06-17 14:41:46 +0530

    exporters and importers modules included in the trunk of the utilities module

---
+++ utilities/exporters/db-exporter/src/test/java/com/techsocialist/utilities/exporters/AppTest.java
@@ -0,0 +1,20 @@
+package com.techsocialist.utilities.exporters;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
