99a76813f12a882cb011aa061a508715aa673510
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-06-28 00:37:43 +0530

    android-apps pom.xml dependency corrected and test cases of okhttp-client module updated

--- utilities/clients/okhttp-client/src/main/java/com/techsocialist/utilities/clients/okhttp/TechSocialistOkHttpRequest.java
+++ utilities/clients/okhttp-client/src/main/java/com/techsocialist/utilities/clients/okhttp/TechSocialistOkHttpRequest.java
@@ -1,9 +1,15 @@
 package com.techsocialist.utilities.clients.okhttp;
 
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class TechSocialistOkHttpRequest {
+
+    public static final MediaType HTML = MediaType.parse("text/html; charset=utf-8");
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private Request request;
 
@@ -32,5 +38,21 @@ public class TechSocialistOkHttpRequest {
         Request.Builder builder = this.request.newBuilder();
         this.request = builder.header(name, value).build();
         return this;
+    }
+
+    public TechSocialistOkHttpRequest addBody(MediaType mediaType, String requestBody) {
+        RequestBody body = RequestBody.create(mediaType, requestBody);
+        HttpUrl.Builder urlBuilder = this.request.url().newBuilder();
+        String url = urlBuilder.build().toString();
+        this.request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        return this;
+    }
+
+    public TechSocialistOkHttpRequest addJsonBody(String requestBody) {
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        return this.addBody(mediaType, requestBody);
     }
 }
