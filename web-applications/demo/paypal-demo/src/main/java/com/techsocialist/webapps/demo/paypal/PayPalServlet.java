454ff6cd19b513e07d1c3ea958137581247b8965
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-07-05 00:07:57 +0530

    request logged and servlet-api scoped to provided

--- web-applications/demo/paypal-demo/src/main/java/com/techsocialist/webapps/demo/paypal/PayPalServlet.java
+++ web-applications/demo/paypal-demo/src/main/java/com/techsocialist/webapps/demo/paypal/PayPalServlet.java
@@ -5,11 +5,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class PayPalServlet extends HttpServlet {
 
+    public static final Logger LOGGER = Logger.getLogger(PayPalServlet.class.getName());
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.service(request, response);
+        this.logRequestDetails(request);
+    }
+
+    private void logRequestDetails(HttpServletRequest request){
+
+        StringBuilder logStringBuilder = new StringBuilder();
+        logStringBuilder.append("-------------------------------------------------+").append("\n");
+        logStringBuilder.append(String.format("  Protocol Info     := %s",request.getProtocol())).append("\n");
+        logStringBuilder.append(String.format("  Method Info       := %s",request.getMethod())).append("\n");
+        logStringBuilder.append(String.format("  Scheme Info       := %s",request.getScheme())).append("\n");
+        logStringBuilder.append(String.format("  Context Path Info := %s",request.getContextPath())).append("\n");
+        logStringBuilder.append(String.format("  URI Info          := %s",request.getRequestURI())).append("\n");
+        logStringBuilder.append(String.format("  Path Info         := %s",request.getPathInfo())).append("\n");
+
+        logStringBuilder.append(String.format("  Servlet Path Info := %s",request.getServletPath())).append("\n");
+        logStringBuilder.append(String.format("  Content Type Info := %s",request.getContentType())).append("\n");
+        logStringBuilder.append("+------------------------------------------------------+");
+
+        LOGGER.info(logStringBuilder.toString());
     }
 }
