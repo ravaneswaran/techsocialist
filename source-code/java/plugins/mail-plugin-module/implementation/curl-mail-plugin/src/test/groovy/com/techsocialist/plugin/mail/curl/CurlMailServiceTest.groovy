76db0e60ecbd575283006762932be885a20bbca1
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-09 16:14:35 +0530

    curl mail plugin tested with attachment

--- source-code/java/plugins/mail-plugin-module/implementation/curl-mail-plugin/src/test/groovy/com/techsocialist/plugin/mail/curl/CurlMailServiceTest.groovy
+++ source-code/java/plugins/mail-plugin-module/implementation/curl-mail-plugin/src/test/groovy/com/techsocialist/plugin/mail/curl/CurlMailServiceTest.groovy
@@ -34,4 +34,70 @@ class CurlMailServiceTest extends Specification{
             IMailService.STATUS_SUCCESS == curlMailService.getStatus()
     }
 
+    def "test sending a mail when from email address is null or empty to to@to.com"(){
+        setup :
+            def curlMailService = new CurlMailService()
+            def from = null
+            def to = "to@to.com"
+            def host = "localhost"
+            def port = "1025"
+            def userName = ""
+            def password = ""
+            curlMailService.connect(host, port, userName, password)
+        when :
+            curlMailService.sendMail(from, to, "Test Subject", "Test Message")
+        then :
+            thrown RuntimeException
+    }
+
+    def "test sending a mail when from email address is from@from.com and to email address is null or empty"(){
+        setup :
+            def curlMailService = new CurlMailService()
+            def from = "from@from.com"
+            def to = null
+            def host = "localhost"
+            def port = "1025"
+            def userName = ""
+            def password = ""
+            curlMailService.connect(host, port, userName, password)
+        when :
+            curlMailService.sendMail(from, to, "Test Subject", "Test Message")
+        then :
+            thrown RuntimeException
+    }
+
+    def "test when sending a mail from from@from.com to to@to.com with one carbon copy"(){
+        setup :
+            def curlMailService = new CurlMailService()
+            def from = "from@from.com"
+            def to = "to@to.com"
+            String[] ccs = ["cc@cc.com"]
+            def host = "localhost"
+            def port = "1025"
+            def userName = ""
+            def password = ""
+            curlMailService.connect(host, port, userName, password)
+        when :
+            curlMailService.sendMail(from, to, ccs, "Test Subject", "Test Message")
+        then :
+            IMailService.STATUS_SUCCESS == curlMailService.getStatus()
+    }
+
+    def "test when sending a mail from from@from.com to to@to.com with one blind carbon copy"(){
+        setup :
+            def curlMailService = new CurlMailService()
+            def from = "from@from.com"
+            String[] tos = ["to@to.com"]
+            String[] bccs = ["bcc@bcc.com"]
+            def host = "localhost"
+            def port = "1025"
+            def userName = ""
+            def password = ""
+            curlMailService.connect(host, port, userName, password)
+        when :
+            curlMailService.sendMail(from, tos, null, bccs, null, "Test Subject", "Test Message")
+        then :
+            IMailService.STATUS_SUCCESS == curlMailService.getStatus()
+    }
+
 }
--- source-code/java/plugins/mail-plugin-module/mail-plugin-api/src/main/java/com/techsocialist/plugin/mail/service/impl/AbstractMailService.java
+++ source-code/java/plugins/mail-plugin-module/mail-plugin-api/src/main/java/com/techsocialist/plugin/mail/service/impl/AbstractMailService.java
@@ -6,22 +6,34 @@ public abstract class AbstractMailService implements IMailService {
 
     @Override
     public void sendMail(String from, String to, String subject, String message) throws Exception {
-        String[] tos = new String[1];
-        tos[0] = to;
-        this.sendMail(from, tos, null, null, null, subject, message);
+        if(null != to && !to.isEmpty()){
+            String[] tos = new String[1];
+            tos[0] = to;
+            this.sendMail(from, tos, null, null, null, subject, message);
+        } else {
+            this.sendMail(from, null, null, null, null, subject, message);
+        }
     }
 
     @Override
     public void sendMail(String from, String to, String[] ccs, String subject, String message) throws Exception {
-        String[] tos = new String[1];
-        tos[0] = to;
-        this.sendMail(from, tos, ccs, null, null, subject, message);
+        if(null != to && !to.isEmpty()) {
+            String[] tos = new String[1];
+            tos[0] = to;
+            this.sendMail(from, tos, ccs, null, null, subject, message);
+        } else {
+            this.sendMail(from, null, ccs, null, null, subject, message);
+        }
     }
 
     @Override
     public void sendMail(String from, String to, String[] ccs, String[] bccs, String subject, String message) throws Exception {
-        String[] tos = new String[1];
-        tos[0] = to;
-        this.sendMail(from, tos, ccs, bccs, null, subject, message);
+        if(null != to && !to.isEmpty()) {
+            String[] tos = new String[1];
+            tos[0] = to;
+            this.sendMail(from, tos, ccs, bccs, null, subject, message);
+        } else {
+            this.sendMail(from, null, ccs, bccs, null, subject, message);
+        }
     }
 }
