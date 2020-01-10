eb1cbead873721ab3d92f3a48d0c7d3c8d6066d3
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-10 23:46:40 +0530

    unit test case included

--- source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/test/groovy/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeServiceTest.groovy
+++ source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/test/groovy/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeServiceTest.groovy
@@ -2,23 +2,27 @@ package com.techsocialist.plugin.qrcode.barcode4j
 
 import spock.lang.Specification
 
-class Barcode4JQRCodeServiceTest extends Specification{
+class Barcode4JQRCodeServiceTest extends Specification {
 
-    def "test creating a qrcode image" (){
+    def "test creating a qrcode image"() {
         setup:
         def qrCodeService = new Barcode4JQRCodeService()
+
         when:
         def result = qrCodeService.createImage("test data", "UTF-8", 100, 100)
+
         then:
         result != null
     }
 
-    def "test scanning a qrcode image" (){
+    def "test scanning a qrcode image"() {
         setup:
         def qrCodeService = new Barcode4JQRCodeService()
         def image = qrCodeService.createImage("test data", "UTF-8", 100, 100)
+
         when:
         def scannedResult = qrCodeService.scanImage(image)
+
         then:
         null != scannedResult && "test data" == scannedResult
     }
