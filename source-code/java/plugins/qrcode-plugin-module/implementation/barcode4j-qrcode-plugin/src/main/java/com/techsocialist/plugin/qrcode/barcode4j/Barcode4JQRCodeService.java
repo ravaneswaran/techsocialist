205fb2d82603cc36c64baa612ee7177a06c5b5ed
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-10 23:45:05 +0530

    unit test case included

--- source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/main/java/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeService.java
+++ source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/main/java/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeService.java
@@ -1,18 +1,23 @@
 package com.techsocialist.plugin.qrcode.barcode4j;
 
-import com.techsocialist.plugin.qrcode.service.impl.AbstractQRCodeService;
+import com.techsocialist.plugin.qrcode.google.zxing.GoogleZxingQRCodeService;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
-public class Barcode4JQRCodeService extends AbstractQRCodeService {
+public class Barcode4JQRCodeService extends GoogleZxingQRCodeService {
 
     @Override
     public BufferedImage createImage(String data, String charset, int imageWidth, int imageHeight) throws Exception {
-        return null;
+        File file =  QRCode.from(data).withCharset(charset).withSize(imageWidth, imageHeight).to(ImageType.PNG).file();
+        return ImageIO.read(file);
     }
 
     @Override
     public String scanImage(BufferedImage bufferedImage) throws Exception {
-        return null;
+        return super.scanImage(bufferedImage);
     }
 }
--- source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/test/groovy/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeServiceTest.groovy
+++ source-code/java/plugins/qrcode-plugin-module/implementation/barcode4j-qrcode-plugin/src/test/groovy/com/techsocialist/plugin/qrcode/barcode4j/Barcode4JQRCodeServiceTest.groovy
@@ -3,4 +3,23 @@ package com.techsocialist.plugin.qrcode.barcode4j
 import spock.lang.Specification
 
 class Barcode4JQRCodeServiceTest extends Specification{
+
+    def "test creating a qrcode image" (){
+        setup:
+        def qrCodeService = new Barcode4JQRCodeService()
+        when:
+        def result = qrCodeService.createImage("test data", "UTF-8", 100, 100)
+        then:
+        result != null
+    }
+
+    def "test scanning a qrcode image" (){
+        setup:
+        def qrCodeService = new Barcode4JQRCodeService()
+        def image = qrCodeService.createImage("test data", "UTF-8", 100, 100)
+        when:
+        def scannedResult = qrCodeService.scanImage(image)
+        then:
+        null != scannedResult && "test data" == scannedResult
+    }
 }
