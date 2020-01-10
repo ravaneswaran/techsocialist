aaa92e70ebd8affe9f4c7e8918bff9f7d8de56eb
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-10 11:06:39 +0530

    twilio properties refactored

--- source-code/java/plugins/sms-plugin-module/implementation/twilio-sms-plugin/src/test/groovy/com/techsocialist/plugin/sms/twilio/TwilioSMSServiceTest.groovy
+++ source-code/java/plugins/sms-plugin-module/implementation/twilio-sms-plugin/src/test/groovy/com/techsocialist/plugin/sms/twilio/TwilioSMSServiceTest.groovy
@@ -1,5 +1,6 @@
 package com.techsocialist.plugin.sms.twilio
 
+import com.techsocialist.plugin.sms.service.api.ITwilioSMSService
 import com.techsocialist.plugin.sms.service.util.SMSUtil
 import spock.lang.Ignore
 import spock.lang.Specification
@@ -9,27 +10,27 @@ class TwilioSMSServiceTest extends Specification{
     def getProperties(){
         Properties properties = new Properties()
 
-        properties.put("sms.account.id", "ACcd1bfb5fb1226482df6fe075fbb3af4f")
-        properties.put("sms.account.auth.token", "0e4c956ff5d748ddf248faf65d826a1f")
-        properties.put("sms.from.mobile.number", "+16232422765")
+        properties.put("sms.twilio.account.sid", "ACcd1bfb5fb1226482df6fe075fbb3af4f")
+        properties.put("sms.twilio.auth.token", "0e4c956ff5d748ddf248faf65d826a1f")
+        properties.put("sms.twilio.from", "+16232422765")
 
         properties
     }
 
     def "test initialize sms service when both are valid"(){
         setup:
-            def twilioSMSService = new TwilioSMSService()
+            def smsService = new TwilioSMSService()
         when :
-            def result = twilioSMSService.initialize("123", "456")
+            def result = smsService.initialize("123", "456")
         then :
             null == result
     }
 
     def "test initialize sms service when account sid is null or empty"(){
         setup:
-            def twilioSMSService = new TwilioSMSService()
+            def smsService = new TwilioSMSService()
         when :
-            def result = twilioSMSService.initialize(null, "456")
+            def result = smsService.initialize(null, "456")
         then :
             RuntimeException rt =  thrown()
             "account SID cannot be null or empty..." == rt.getMessage()
@@ -37,9 +38,9 @@ class TwilioSMSServiceTest extends Specification{
 
     def "test initialize sms service when auth token is null or empty"(){
         setup:
-            def twilioSMSService = new TwilioSMSService()
+            def smsService = new TwilioSMSService()
         when :
-            def result = twilioSMSService.initialize("123", null)
+            def result = smsService.initialize("123", null)
         then :
             RuntimeException rt =  thrown()
             "authToken cannot be null or empty..." == rt.getMessage()
@@ -48,10 +49,10 @@ class TwilioSMSServiceTest extends Specification{
     @Ignore
     def "test sending Hello World message to mobile number +919894794196"(){
         setup:
-            def twilioSMSService = new TwilioSMSService()
-            twilioSMSService.initialize(properties.get("sms.account.id"), properties.get("sms.account.auth.token"))
+            def smsService = new TwilioSMSService()
+            smsService.initialize(properties.get("sms.twilio.account.sid"), properties.get("sms.twilio.auth.token"))
         when :
-            String result = twilioSMSService.sendSMS(properties.get("sms.from.mobile.number"), "+919894794196", "Hello World")
+            String result = smsService.sendSMS(properties.get("sms.from."), "+919894794196", "Hello World")
         then :
             null != result && !result.isEmpty()
     }
@@ -59,20 +60,22 @@ class TwilioSMSServiceTest extends Specification{
     @Ignore
     def "test sending Hello World message to mobile number +919894794196 using service"(){
         setup:
-            def twilioSMSService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
-            twilioSMSService.initialize(properties.get("sms.account.id"), properties.get("sms.account.auth.token"))
+            def smsService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
+            ITwilioSMSService twilioSMSService = smsService
+            twilioSMSService.initialize(properties.get("sms.twilio.account.sid"), properties.get("sms.twilio.auth.token"))
         when :
-            String result = twilioSMSService.sendSMS(properties.get("sms.from.mobile.number"), "+919894794196", "Hello World")
+            String result = smsService.sendSMS(properties.get("sms.twilio.from"), "+919894794196", "Hello World")
         then :
             null != result && !result.isEmpty()
     }
 
     def "test sending Hello World message to mobile number when from mobile number is null or empty"(){
         setup:
-            def twilioSMSService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
-            twilioSMSService.initialize(properties.get("sms.account.id"), properties.get("sms.account.auth.token"))
+            def smsService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
+            ITwilioSMSService twilioSMSService = smsService
+            twilioSMSService.initialize(properties.get("sms.twilio.account.sid"), properties.get("sms.twilio.auth.token"))
         when :
-            String result = twilioSMSService.sendSMS(null, "+919894794196", "Hello World")
+            smsService.sendSMS(null, "+919894794196", "Hello World")
         then :
             RuntimeException re = thrown()
             "from(SMS Source) cannot be null or empty..." == re.getMessage()
@@ -80,10 +83,11 @@ class TwilioSMSServiceTest extends Specification{
 
     def "test sending Hello World message to mobile number when to mobile number is null or empty"(){
         setup:
-            def twilioSMSService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
-            twilioSMSService.initialize(properties.get("sms.account.id"), properties.get("sms.account.auth.token"))
+            def smsService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
+            ITwilioSMSService twilioSMSService = smsService
+            twilioSMSService.initialize(properties.get("sms.twilio.account.sid"), properties.get("sms.twilio.auth.token"))
         when :
-            String result = twilioSMSService.sendSMS(properties.get("sms.from.mobile.number"), null, "Hello World")
+            smsService.sendSMS(properties.get("sms.twilio.from"), null, "Hello World")
         then :
             RuntimeException re = thrown()
             "to(SMS Destination) cannot be null or empty..." == re.getMessage()
@@ -91,10 +95,11 @@ class TwilioSMSServiceTest extends Specification{
 
     def "test sending null or empty message to mobile number +919894794196 using service"(){
         setup:
-            def twilioSMSService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
-            twilioSMSService.initialize(properties.get("sms.account.id"), properties.get("sms.account.auth.token"))
+            def smsService = SMSUtil.getSMSService("com.techsocialist.plugin.sms.twilio.TwilioSMSService")
+            ITwilioSMSService twilioSMSService = smsService
+            twilioSMSService.initialize(properties.get("sms.twilio.account.sid"), properties.get("sms.twilio.auth.token"))
         when :
-            String result = twilioSMSService.sendSMS(properties.get("sms.from.mobile.number"), "+919894794196", null)
+            smsService.sendSMS(properties.get("sms.twilio.from"), "+919894794196", null)
         then :
             RuntimeException re = thrown()
             "message cannot be null or empty..." == re.getMessage()
