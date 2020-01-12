6e2ebf256af46cf65bab5ff0cb07a199cd226548
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-13 00:39:06 +0530

    net banking pament channel request completed with 100% code and branch coverage

---
+++ source-code/java/plugins/payment-gateway-plugin-module/implementation/rest/paytm-pg-rest-plugin/src/main/java/com/techsocialist/plugin/pg/paytm/request/NetBankingPaymentChannelRequest.java
@@ -0,0 +1,39 @@
+package com.techsocialist.plugin.pg.paytm.request;
+
+import org.json.JSONObject;
+
+public class NetBankingPaymentChannelRequest extends AbstractPaytmRequest {
+
+    private String type;
+
+    @Override
+    public String url(boolean production) {
+        return String.format("%s/%s/fetchNBPaymentChannels?mid=%s&orderId=%s", this.getUrlEndPointPrefix(production), this.getVersion(),  this.getMerchantId(), this.getOrderId());
+    }
+
+    @Override
+    public JSONObject dataHead() {
+        JSONObject head = new JSONObject();
+
+        head.put("clientId", this.getClientId());
+        head.put("version", this.getVersion());
+        head.put("requestTimestamp", this.getRequestTimestamp());
+        head.put("channelId", this.getChannelId());
+        head.put("txnToken", this.getTransactionToken());
+
+        return head;
+    }
+
+    @Override
+    public JSONObject dataBody() {
+        JSONObject body = new JSONObject();
+        body.put("type", this.type);
+
+        return body;
+    }
+
+    public NetBankingPaymentChannelRequest setType(String type) {
+        this.type = type;
+        return this;
+    }
+}
---
+++ source-code/java/plugins/payment-gateway-plugin-module/implementation/rest/paytm-pg-rest-plugin/src/test/groovy/com/techsocialist/plugin/pg/paytm/request/NetBankingPaymentChannelRequestTest.groovy
@@ -0,0 +1,68 @@
+package com.techsocialist.plugin.pg.paytm.request
+
+import spock.lang.Specification
+
+class NetBankingPaymentChannelRequestTest extends Specification{
+
+    def "test data head"() {
+        setup:
+        NetBankingPaymentChannelRequest paytmRequest = new NetBankingPaymentChannelRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setMerchantKey("mer-key")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setClientId("client-001")
+        paytmRequest.setVersion("v1")
+        paytmRequest.setChannelId("WEB")
+
+        then:
+        null != paytmRequest.dataHead()
+    }
+
+    def "test data body"() {
+        setup:
+        NetBankingPaymentChannelRequest paytmRequest = new NetBankingPaymentChannelRequest()
+
+        when:
+        paytmRequest.setType("test")
+
+        then:
+        null != paytmRequest.dataBody()
+    }
+
+
+    def "test staging url construction"() {
+        setup:
+        NetBankingPaymentChannelRequest paytmRequest = new NetBankingPaymentChannelRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setClientId("client-001")
+        paytmRequest.setVersion("v1")
+        paytmRequest.setChannelId("WEB")
+        paytmRequest.setTransactionToken("txn-001")
+
+        then:
+        "https://securegw-stage.paytm.in/theia/api/v1/fetchNBPaymentChannels?mid=mer-001&orderId=order-001" == paytmRequest.url(false)
+
+    }
+
+    def "test production url construction"() {
+        setup:
+        NetBankingPaymentChannelRequest paytmRequest = new NetBankingPaymentChannelRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setClientId("client-001")
+        paytmRequest.setVersion("v1")
+        paytmRequest.setChannelId("WEB")
+        paytmRequest.setTransactionToken("txn-001")
+
+        then:
+        "https://securegw.paytm.in/theia/api/v1/fetchNBPaymentChannels?mid=mer-001&orderId=order-001" == paytmRequest.url(true)
+
+    }
+}
