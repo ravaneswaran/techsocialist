d636804398c2dcc8aff4ac1b9d2e5bbf8814882f
Ravaneswaran Chinnsamy <ravaneswaran.chinnasamy@niivsoftware.com>
2020-01-13 12:37:34 +0530

    refund request completed with 100% code and branch coverage

---
+++ source-code/java/plugins/payment-gateway-plugin-module/implementation/rest/paytm-pg-rest-plugin/src/test/groovy/com/techsocialist/plugin/pg/paytm/request/RefundRequestTest.groovy
@@ -0,0 +1,64 @@
+package com.techsocialist.plugin.pg.paytm.request
+
+import spock.lang.Specification
+
+class RefundRequestTest extends Specification{
+
+    def "test data head"() {
+        setup:
+        RefundRequest paytmRequest = new RefundRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setMerchantKey("mer-key")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setVersion("v1")
+        paytmRequest.setChannelId("WEB")
+
+        then:
+        null != paytmRequest.dataHead()
+    }
+
+    def "test data body"() {
+        setup:
+        RefundRequest paytmRequest = new RefundRequest()
+
+        when:
+        paytmRequest.setAmountToRefund("1000.00").setRefundId("refund-id").setTransactionId("txn-id").setTransactionType("txn-type")
+
+        then:
+        null != paytmRequest.dataBody()
+    }
+
+
+    def "test staging url construction"() {
+        setup:
+        RefundRequest paytmRequest = new RefundRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setMerchantKey("mer-key")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setVersion("v1")
+
+        then:
+        "https://securegw-stage.paytm.in/refund/apply" == paytmRequest.url(false)
+
+    }
+
+    def "test production url construction"() {
+        setup:
+        RefundRequest paytmRequest = new RefundRequest()
+
+        when:
+        paytmRequest.setMerchantId("mer-001")
+        paytmRequest.setMerchantKey("mer-key")
+        paytmRequest.setOrderId("order-001")
+        paytmRequest.setVersion("v1")
+
+        then:
+        "https://securegw.paytm.in/refund/apply" == paytmRequest.url(true)
+
+    }
+
+}
