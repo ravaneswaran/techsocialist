14328c6dee85091ccac6226d4bd81a311b662ecb
Ravaneswaran Chinnasamy <ravaneswaran@gmail.com>
2020-05-09 14:32:48 +0530

    resouce folder populated

--- source-code/liferay/modules/techsocialist-core/foundation-module/src/main/java/com/techsocialist/foundation/portlet/HelloWorldPortlet.java
+++ source-code/liferay/modules/techsocialist-core/foundation-module/src/main/java/com/techsocialist/foundation/portlet/HelloWorldPortlet.java
@@ -8,12 +8,12 @@ import javax.portlet.Portlet;
 @Component(
         immediate = true,
         property = {
-                "com.liferay.portlet.display-category=category.sample",
+                "com.liferay.portlet.display-category=TS-Foundation",
                 "com.liferay.portlet.header-portlet-css=/css/main.css",
                 "com.liferay.portlet.instanceable=true",
-                "javax.portlet.display-name=HelloWorld",
+                "javax.portlet.display-name=Hello World",
                 "javax.portlet.init-param.template-path=/",
-                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.init-param.view-template=/hello-world/view.jsp",
                 "javax.portlet.name=" + HelloWorldPortletKeys.HELLO_WORLD,
                 "javax.portlet.resource-bundle=content.Language",
                 "javax.portlet.security-role-ref=power-user,user"
---
+++ source-code/liferay/modules/techsocialist-core/foundation-module/src/main/resources/META-INF/resources/hello-world/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/hello-world/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="helloworld.caption"/></b>
+</p>
