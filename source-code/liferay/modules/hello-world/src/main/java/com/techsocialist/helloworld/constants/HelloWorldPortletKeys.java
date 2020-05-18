9818034e973be2c14479d867c26f23f5d8525f6e
Ravaneswaran Chinnasamy <ravaneswaran@gmail.com>
2020-05-18 11:43:24 +0530

    liferay folder added

---
+++ source-code/liferay/modules/hello-world/src/main/java/com/techsocialist/helloworld/constants/HelloWorldPortletKeys.java
@@ -0,0 +1,11 @@
+package com.techsocialist.helloworld.constants;
+
+/**
+ * @author ravaneswaran
+ */
+public class HelloWorldPortletKeys {
+
+	public static final String HELLOWORLD =
+		"com_techsocialist_helloworld_HelloWorldPortlet";
+
+}
