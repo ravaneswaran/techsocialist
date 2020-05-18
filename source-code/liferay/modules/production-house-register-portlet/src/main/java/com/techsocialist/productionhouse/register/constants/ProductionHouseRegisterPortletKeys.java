6b11c28715d87e695a64d596797e1f1100392056
Ravaneswaran Chinnasamy <ravaneswaran@gmail.com>
2020-05-18 15:13:15 +0530

    ProductionHouseRegistrationPortlet name renamed

---
+++ source-code/liferay/modules/production-house-register-portlet/src/main/java/com/techsocialist/productionhouse/register/constants/ProductionHouseRegisterPortletKeys.java
@@ -0,0 +1,11 @@
+package com.techsocialist.productionhouse.register.constants;
+
+/**
+ * @author ravaneswaran
+ */
+public class ProductionHouseRegisterPortletKeys {
+
+	public static final String PRODUCTIONHOUSEREGISTER =
+		"com_techsocialist_productionhouse_register_ProductionHouseRegisterPortlet";
+
+}
--- source-code/liferay/modules/production-house-register-portlet/src/main/java/com/techsocialist/productionhouse/register/portlet/ProductionHouseRegisterPortlet.java
+++ source-code/liferay/modules/production-house-register-portlet/src/main/java/com/techsocialist/productionhouse/register/portlet/ProductionHouseRegisterPortlet.java
@@ -1,6 +1,6 @@
 package com.techsocialist.productionhouse.register.portlet;
 
-import com.techsocialist.productionhouse.register.constants.ProductionHouseRegistrationPortletKeys;
+import com.techsocialist.productionhouse.register.constants.ProductionHouseRegisterPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
@@ -17,14 +17,14 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=ProductionHouseRegistration",
+		"javax.portlet.display-name=ProductionHouseRegister",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + ProductionHouseRegistrationPortletKeys.PRODUCTIONHOUSEREGISTRATION,
+		"javax.portlet.name=" + ProductionHouseRegisterPortletKeys.PRODUCTIONHOUSEREGISTER,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
-public class ProductionHouseRegistrationPortlet extends MVCPortlet {
+public class ProductionHouseRegisterPortlet extends MVCPortlet {
 }
