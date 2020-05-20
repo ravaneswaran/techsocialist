31ba5e1103f54c908d361ea9766c51d1973e7772
ravaneswaran <ravaneswaran@gmail.com>
2020-05-20 16:42:11 +0530

    control panel app renamed

---
+++ source-code/liferay/modules/host-platform-share-configuration-cp-portlet/src/main/java/com/techsocialist/controlpanel/constants/HostPlatformShareConfigurationPanelCategoryKeys.java
@@ -0,0 +1,10 @@
+package com.techsocialist.controlpanel.constants;
+
+/**
+ * @author ravaneswaran
+ */
+public class HostPlatformShareConfigurationPanelCategoryKeys {
+
+	public static final String CONTROL_PANEL_CATEGORY = "HostPlatformShareConfiguration";
+
+}
---
+++ source-code/liferay/modules/host-platform-share-configuration-cp-portlet/src/main/java/com/techsocialist/controlpanel/constants/HostPlatformShareConfigurationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.techsocialist.controlpanel.constants;
+
+/**
+ * @author ravaneswaran
+ */
+public class HostPlatformShareConfigurationPortletKeys {
+
+	public static final String HOSTPLATFORMSHARECONFIGURATION =
+		"com_techsocialist_controlpanel_HostPlatformShareConfigurationPortlet";
+
+}
